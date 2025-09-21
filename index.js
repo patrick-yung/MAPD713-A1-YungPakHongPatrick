a44df4a51b2e3c771ed418cd4fae63e776442882
Patrick Yung <patrickyyung@gmail.com>
2025-09-21 17:45:32 -0400

    Added posted method description for assignment, courses and student

--- index.js
+++ index.js
@@ -19,60 +19,122 @@ let restify = require('restify')
   console.log('********************')
   console.log(' /student')
   console.log(' /course')  
+  console.log(' /assignment')
+  console.log('********************')
 })
 
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
 
-// Get a single user by their user id
-server.get('/users/:id', (req, res, next) => {
-  
+/************************************************************/
+
+
+// Get a list of all students/assignments/courses
+server.get('/students', (req, res, next) => {
+  console.log('GET /students params=>' + JSON.stringify(req.params));
+  studentsSave.find({}, function (error, users) {
+    res.send(users)
+  })
 })
 
-// Create a new student user
+server.get('/courses', (req, res, next) => {
+  console.log('GET /students params=>' + JSON.stringify(req.params));
+  coursesSave.find({}, function (error, users) {
+    res.send(users)
+  })
+})
+server.get('/assignment', (req, res, next) => {
+  console.log('GET /students params=>' + JSON.stringify(req.params));
+  assignmentsSave.find({}, function (error, users) {
+    res.send(users)
+  })
+})
+/************************************************************/
+// Create a new student/course/assignment user
+
+let numberOfStudents = 0;
+let numberOfCourses = 0;
+let numberOfAssignments = 0;
+
 server.post('/students', function (req, res, next) {
   console.log('POST /students body=>' + JSON.stringify(req.body));
-  if (req.body.id === undefined ) {
+  if( req.body.studentId === undefined ) {
     return next(new errors.BadRequestError('student id must be supplied'))
+  } else if (req.body.name === undefined) {
+    return next(new errors.BadRequestError('student name must be supplied'))
+  } else if (req.body.address === undefined) {
+    return next(new errors.BadRequestError('student address must be supplied'))
+  }else if (req.body.phone === undefined) {
+    return next(new errors.BadRequestError('student phone must be supplied'))
   }
+
+  numberOfStudents++;
+
   let newStudent = {
-    id: req.body.id, 
+    id: numberOfStudents,
+    studentid: req.body.studentId,
+    studentname: req.body.name,
+    studentaddress: req.body.address,
+    studentphone: req.body.phone
   }
+  console.log('Final Stage');
+
   studentsSave.create( newStudent, function (error, user) {
   if (error) return next(new Error(JSON.stringify(error.errors)))
     res.send(201, user)
   })
 })
 
-// Create a new course user
 server.post('/courses', function (req, res, next) {
   console.log('POST /courses body=>' + JSON.stringify(req.body));
-  if (req.body.id === undefined ) {
+  if (req.body.courseCode === undefined ) {
     return next(new errors.BadRequestError('course id must be supplied'))
+  }else if (req.body.shortDescription === undefined ) {
+    return next(new errors.BadRequestError('course shortDescription must be supplied'))
+  }else if (req.body.fullDescription === undefined ) {
+    return next(new errors.BadRequestError('course fullDescription must be supplied'))
   }
+ 
+  numberOfCourses++;
+
   let newCourse = {
-    id: req.body.id, 
+    id: numberOfCourses,
+    courseCode: req.body.courseCode,
+    shortDescription: req.body.shortDescription,
+    fullDescription: req.body.fullDescription 
   }
+  
   coursesSave.create( newCourse, function (error, user) {
   if (error) return next(new Error(JSON.stringify(error.errors)))
     res.send(201, user)
   })
 })
 
-// Create a new assignment user
 server.post('/assignments', function (req, res, next) {
   console.log('POST /assignments body=>' + JSON.stringify(req.body));
-  if (req.body.id === undefined ) {
+  if (req.body.assignmentCode === undefined ) {
     return next(new errors.BadRequestError('assignment id must be supplied'))
+  } else if (req.body.dueDate === undefined ) {
+    return next(new errors.BadRequestError('assignment dueDate must be supplied'))
+  } else if (req.body.title === undefined ) {
+    return next(new errors.BadRequestError('assignment title must be supplied'))
   }
+
+  numberOfAssignments++;
+
   let newAssignment = {
-    id: req.body.id, 
+    id: numberOfAssignments,
+    courseCode: req.body.assignmentCode,
+    dueDate: req.body.dueDate,
+    title: req.body.title 
   }
   assignmentsSave.create( newAssignment, function (error, user) {
   if (error) return next(new Error(JSON.stringify(error.errors)))
     res.send(201, user)
   })
 })
+
+/************************************************************/
 
 
 // Update a user by their id
