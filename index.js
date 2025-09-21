d7cf77973f80dbb626cf59fb20f7a1d36a1fb3a6
Patrick Yung <patrickyyung@gmail.com>
2025-09-21 17:22:50 -0400

    Added post method for students, course and assignement

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let errors = require('restify-errors');
 let restify = require('restify')
 
   // Get a persistence engine for the users
-  , usersSave = require('save')('users')
+  , studentsSave = require('save')('students')
+  , coursesSave = require('save')('courses')
+  , assignmentsSave = require('save')('assignments')
 
   // Create the restify server
   , server = restify.createServer({ name: SERVER_NAME})
@@ -22,83 +24,64 @@ let restify = require('restify')
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
 
-server.get('/users', (req, res, next) => {
-
-  usersSave.find({}, function (error, users) {
-
-    // Return all of the users in the system
-    res.send(users)
-  })
-})
-
 // Get a single user by their user id
 server.get('/users/:id', (req, res, next) => {
-  console.log('GET /users/:id params=>' + JSON.stringify(req.params));
-
-  // Find a single user by their id within save
-  usersSave.findOne({ _id: req.params.id }, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new Error(JSON.stringify(error.errors)))
-
-    if (user) {
-      // Send the user if no issues
-      res.send(user)
-    } else {
-      // Send 404 header if the user doesn't exist
-      res.send(404)
-    }
-  })
+  
 })
 
-// Create a new user
-server.post('/users', function (req, res, next) {
-
-  if (req.params.id == "students") {
-
-
-  }else if (req.params.id == "courses") { 
-
-
-  } else if (req.params.id == "assignments"){
-
-
+// Create a new student user
+server.post('/students', function (req, res, next) {
+  console.log('POST /students body=>' + JSON.stringify(req.body));
+  if (req.body.id === undefined ) {
+    return next(new errors.BadRequestError('student id must be supplied'))
   }
-
-  usersSave.create( newUser, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new Error(JSON.stringify(error.errors)))
-
-    // Send the user if no issues
+  let newStudent = {
+    id: req.body.id, 
+  }
+  studentsSave.create( newStudent, function (error, user) {
+  if (error) return next(new Error(JSON.stringify(error.errors)))
     res.send(201, user)
   })
 })
 
-// Update a user by their id
-server.put('/users/:id', function (req, res, next) {
- 
-  usersSave.update(newUser, true, function (error, user) {    
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new Error(JSON.stringify(error.errors)))
-
-    // Send a 200 OK response
-    res.send(200, user)
+// Create a new course user
+server.post('/courses', function (req, res, next) {
+  console.log('POST /courses body=>' + JSON.stringify(req.body));
+  if (req.body.id === undefined ) {
+    return next(new errors.BadRequestError('course id must be supplied'))
+  }
+  let newCourse = {
+    id: req.body.id, 
+  }
+  coursesSave.create( newCourse, function (error, user) {
+  if (error) return next(new Error(JSON.stringify(error.errors)))
+    res.send(201, user)
   })
+})
+
+// Create a new assignment user
+server.post('/assignments', function (req, res, next) {
+  console.log('POST /assignments body=>' + JSON.stringify(req.body));
+  if (req.body.id === undefined ) {
+    return next(new errors.BadRequestError('assignment id must be supplied'))
+  }
+  let newAssignment = {
+    id: req.body.id, 
+  }
+  assignmentsSave.create( newAssignment, function (error, user) {
+  if (error) return next(new Error(JSON.stringify(error.errors)))
+    res.send(201, user)
+  })
+})
+
+
+// Update a user by their id
+server.put('/students/:id', function (req, res, next) { 
 })
 
 // Delete user with the given id
 server.del('/users/:id', function (req, res, next) {
-  console.log('POST /users params=>' + JSON.stringify(req.params));
-  // Delete the user with the persistence engine
-  usersSave.delete(req.params.id, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new Error(JSON.stringify(error.errors)))
-
-    // Send a 204 response
-    res.send(204)
-  })
+ 
 })
 
 
